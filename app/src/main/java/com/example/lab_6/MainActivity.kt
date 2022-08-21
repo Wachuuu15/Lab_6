7fe39ddaae9ca5cb24b1894c86ffd9fc2bd903bd
wachuuu <88355602+Wachuuu15@users.noreply.github.com>
2022-08-20 18:06:20 -0600

    create principal

--- app/src/main/java/com/example/lab_6/MainActivity.kt
+++ app/src/main/java/com/example/lab_6/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.lab_6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNav: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNav = findViewById(R.id.bottomNav_bottomNavActivity)
+        setListeners()
     }
+
+    private fun setListeners() {
+        bottomNav.setOnItemSelectedListener {
+            // Dependiendo el item del menu, mostramos el fragment deseado
+            when(it.itemId) {
+                R.id.menu_option_home -> TODO()
+                R.id.menu_option_search -> TODO()
+                R.id.menu_option_library -> TODO()
+            }
+            true
+        }
+    }
+
 }
